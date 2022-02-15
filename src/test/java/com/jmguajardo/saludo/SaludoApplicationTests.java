f9fb8b1a2c049ee9188650528ebeaa0c9aa2508a
Martin Guajardo <e-jmguajardo@neoris.com>
2022-02-14 18:09:23 -0600

    Init commit v2

---
+++ src/test/java/com/jmguajardo/saludo/SaludoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmguajardo.saludo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
