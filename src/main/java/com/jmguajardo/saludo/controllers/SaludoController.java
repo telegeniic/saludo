790757cdd4c731fd468f75d06c50ed89fa13a66d
Martin Guajardo <e-jmguajardo@neoris.com>
2022-02-15 18:13:40 -0600

    typo correcting

--- src/main/java/com/jmguajardo/saludo/controllers/SaludoController.java
+++ src/main/java/com/jmguajardo/saludo/controllers/SaludoController.java
@@ -6,6 +6,7 @@ import com.jmguajardo.saludo.models.responses.SaludoResponse;
 import com.jmguajardo.saludo.services.SaludoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/saludo/")
 public class SaludoController {
     
